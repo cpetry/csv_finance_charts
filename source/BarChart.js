e0c263c86e9c230167508c32ef7cbc589df3d292
Christian Petry <petry2christian@gmail.com>
2022-03-21 21:02:38 +0100

    Fixed bar selection on filtered view, added tab buttons, refactored a lot

--- source/BarChart.js
+++ source/BarChart.js
@@ -1,63 +1,248 @@
-
 class BarChart
 {
-    constructor(){
-        
-    }
+    constructor()
+    {
+        this._defaultLegendClickHandler = Chart.defaults.plugins.legend.onClick;
 
-    setData(groupedCategories){
-        this.data = {
-        labels: labels,
-        datasets: [{Gas: {
-            '12.2017': { values: [Array], sum: -47.13 },
-            '11.2017': { values: [Array], sum: -19.82 },
-            '10.2017': { values: [Array], sum: -19.82 },
-            '09.2017': { values: [Array], sum: -19.82 },
-            '08.2017': { values: [Array], sum: -47.13 },
-            '07.2017': { values: [Array], sum: -19.82 },
-            '06.2017': { values: [Array], sum: -19.82 },
-            '04.2017': { values: [Array], sum: -47.13 },
-            '03.2017': { values: [Array], sum: -19.82 },
-            '02.2017': { values: [Array], sum: -19.82 },
-            '01.2017': { values: [Array], sum: -19.82 }
-          },
-          Orderings: {
-            '12.2017': { values: [Array], sum: -88.91 },
-            '11.2017': { values: [Array], sum: -63.11000000000001 },
-            '10.2017': { values: [Array], sum: -69.34 },
-            '09.2017': { values: [Array], sum: -74.94 },
-            '08.2017': { values: [Array], sum: -108.73 },
-            '07.2017': { values: [Array], sum: -63.11000000000001 },
-            '06.2017': { values: [Array], sum: -77.33999999999999 },
-            '05.2017': { values: [Array], sum: -94.76 },
-            '04.2017': { values: [Array], sum: -95.14 },
-            '03.2017': { values: [Array], sum: -63.11000000000001 },
-            '02.2017': { values: [Array], sum: -69.34 },
-            '01.2017': { values: [Array], sum: -83.71 }
-          }
-        }],
-        };
-
-        this.config = {
+        this._lastClickTime = performance.now()
+        let config = {
             type: 'bar',
-            data: data,
             options: {
-            plugins: {
-                title: {
-                display: true,
-                text: 'Chart.js Bar Chart - Stacked'
+                maintainAspectRatio: false,
+                responsive: true,
+                interaction: {
+                    //intersect: false,
+                    mode: 'x'
                 },
-            },
-            responsive: true,
-            scales: {
-                x: {
-                stacked: true,
+                animation: {
+                    duration: 800,
                 },
-                y: {
-                stacked: true
+                scales: {
+                    x: {
+                        stacked: true
+                    },
+                    y: {
+                        stacked: true
+                    }
+                },
+                parsing: {
+                    xAxisKey: 'date',
+                    yAxisKey: 'sum'
+                },
+                
+                events: ['mousemove', 'mouseout', 'click', 'touchstart', 'touchmove'],
+
+                onClick: (event, clickedElements) => this.ChartClickHandler(event, clickedElements),
+
+                plugins: {
+                    tooltip: {
+                    callbacks: {
+                        afterTitle: this.ToolTipSum
+                        //label: toolTipContent
+                    }
+                    },
+                    legend: {
+                        onClick: this.NewLegendClickHandler,
+                        labels: {
+                            filter: this.LegendFilter,
+                            sort: this.LegendSorting
+                        }
+                    }
                 }
             }
-            }
         };
+        this._chartCtx = document.getElementById('myChart');
+        this._chart = new Chart(this._chartCtx, config);
+    }
+
+    AddListenerOnSelection(action)
+    {
+        this._onSelectionChanged = action;
+    }
+
+    UpdateChart(financeDataPool, valueSign)
+    {
+        this._valueSign = valueSign;
+        let categories = _cfgFile.getCategories()
+        
+        let ignoredCategories = []
+        if (valueSign == ValueSign.TOTAL)
+            ignoredCategories = ["Hausbau"]
+
+        let categorizedSums = financeDataPool.getCategorizedGroupedByMonth(categories, valueSign, /*recreate*/ true, ignoredCategories = ignoredCategories);
+        let labels = financeDataPool.getDateLabels()
+
+        this._chart.options.scales.x.labels = labels
+        this._chart.data.datasets = categorizedSums
+        this._chart.update();
+    }
+
+    ChangeHoverOption(option)
+    {
+        let mode = ""
+        switch(option){
+            case "single":
+                this._chart.options.interaction.intersect = true
+                this._chart.options.interaction.mode = "nearest"
+                break;
+            case "bar":
+            default:
+                this._chart.options.interaction.intersect = true
+                this._chart.options.interaction.mode = "x"
+                break;
+        }
+        this._chart.update();
+    }
+
+    ToolTipSum(tooltipItems)
+    {
+        let sum = 0;    
+        tooltipItems.forEach(function(tooltipItem) {
+            sum += tooltipItem.parsed.y;
+        });
+
+        // round to two decimals
+        sum = Math.round((sum + Number.EPSILON) * 100) / 100
+        return 'Sum: ' + sum;
+    };
+
+    ToolTipContent(tooltipItems)
+    {
+        let label = tooltipItems.dataset.label;
+        let dataOfCategory = tooltipItems.dataset.data[tooltipItems.dataIndex]
+
+        sum = dataOfCategory.sum
+        sum = Math.round((sum + Number.EPSILON) * 100) / 100
+        let details = []
+        details.push(label + ':')
+
+        // sort detail info descending by value
+        dataList = dataOfCategory.values
+        dataList = dataList.sort(function(a, b) {
+            return -(a.value - b.value);
+        });
+        dataList.forEach((entry) => details.push(entry.client + ': ' + entry.value + '\r'))
+
+        return details;
+    }
+
+    ChartClickHandler(event, clickedElements)
+    {
+        if (clickedElements.length === 0) return
+
+        const index = clickedElements[0].index;
+        const datasetIndex = clickedElements[0].datasetIndex;
+
+        let tableData = []
+        if (this._chart.options.interaction.mode == 'x')
+        {
+            let filterDate = this._chart.data.datasets[datasetIndex].data[index].date;
+            if (this._valueSign == ValueSign.TOTAL)
+                tableData = this.GetTableDataFromMultipleDatasets(filterDate, /*onlySums*/ true)
+            else
+                tableData = this.GetTableDataFromMultipleDatasets(filterDate, /*onlySums*/ false)
+        }
+        else 
+        {
+            
+            if (this._valueSign == ValueSign.TOTAL)
+                tableData = this._chart.data.datasets[datasetIndex].data[index]
+            else
+                tableData = this._chart.data.datasets[datasetIndex].data[index].values
+        }
+
+        if(this._onSelectionChanged != undefined)
+            this._onSelectionChanged(tableData);
+    }
+    
+    GetTableDataFromMultipleDatasets(filterDate, onlySums)
+    {
+        let tableData = []
+        for(var i=0; i<this._chart.data.datasets.length; i++) {
+            let isVisible = this._chart.isDatasetVisible(i)
+            if (!isVisible)
+                continue;
+
+            let dataset = this._chart.data.datasets[i]
+            let categoryData = dataset.data.find((data) => data.date == filterDate);
+            if (categoryData == undefined)
+                continue
+
+            let values = []
+            if (onlySums)
+                values = categoryData
+            else
+                values = categoryData.values.filter(x => x != undefined)
+            tableData = tableData.concat(values)
+        }
+        return tableData
+    }
+
+    GetLatestTableData()
+    {
+        return this._tableData
+    }
+
+    NewLegendClickHandler(event, legendItem, legend) 
+    {
+        let now = performance.now()
+        let doubleClicked = false
+        if (now - this._lastClickTime < 200)
+            doubleClicked = true
+        this._lastClickTime = performance.now()
+        
+        const clickedIndex = legendItem.datasetIndex;
+
+        if (!doubleClicked) {
+            this._defaultLegendClickHandler(event, legendItem, legend);
+        } else {
+            const ci = legend.chart;
+
+            var allOthersAreInvisible = true
+            for (var i=0; i < legend.legendItems.length; i++)
+            {
+                if (i != clickedIndex && ci.isDatasetVisible(i)){
+                    allOthersAreInvisible = false
+                    break;
+                }
+            }
+
+            for (var i=0; i < legend.legendItems.length; i++){
+                if (allOthersAreInvisible){
+                    ci.show(i);
+                    legendItem.hidden = false;
+                } else {
+                    ci.hide(i);
+                    legendItem.hidden = true;
+                }
+            }
+
+            ci.show(clickedIndex);
+            legendItem.hidden = false;
+        }
+    }
+
+    
+    LegendSorting(legendItemA, legendItemB, data)
+    {
+        if (legendItemA.text > legendItemB.text)
+            return 1
+        else if (legendItemA.text < legendItemB.text)
+            return -1
+        else
+            return 0;
+    }
+
+    LegendFilter(legendItem, data)
+    {
+        var categoryIndex = legendItem.datasetIndex
+        var dataOfCategory = data.datasets[categoryIndex].data
+        return dataOfCategory.length > 0
+    }
+
+    Reset()
+    {
+        if(this._chart instanceof Chart)
+            this._chart.destroy();
     }
 }
--- source/renderer.js
+++ source/renderer.js
@@ -1,17 +1,30 @@
-const ctx = document.getElementById('myChart');
 var _cfgFile
+var _valueSign = ValueSign.NEGATIVE
+var _financeDataPool = new FinanceDataPool();
+var _table
+var _barChart
+
 window.electron.onConfigLoaded((data) => {
     console.log("Config data received")
     _cfgFile = new CFG_File(data);
 
-    CreateChart();
+    CreateTabs();
+    _barChart = new BarChart();
+    _barChart.AddListenerOnSelection(function (barData) { UpdateTable(barData)})
     CreateTable();
+});
+
+window.electron.onCSVLoaded((data) => {
+    console.log("CSV data received")
+    let csvFile = new CSV_File_DKB(data);
+    _financeDataPool.Add(csvFile);
+    _barChart.UpdateChart(_financeDataPool);
 });
 
 const buttonReload = document.getElementById('buttonReload')
 buttonReload.addEventListener('click', () => {
-    if(_chart instanceof Chart)
-        _chart.destroy();
+    if (_barChart !== undefined)
+        _barChart.Reset();
     if (_financeDataPool !== undefined)
         _financeDataPool.Clear();
     window.electron.onReload()
@@ -19,265 +32,39 @@ buttonReload.addEventListener('click', () => {
 
 const selectHover = document.getElementById('selectHover')
 selectHover.addEventListener('change', () => {
-    console.log(selectHover.value)
-    ChangeHoverOption(selectHover.value)
+    if (_barChart !== undefined)
+        _barChart.ChangeHoverOption(selectHover.value)
 });
 
-const selectFilter = document.getElementById('selectFilter')
-selectFilter.addEventListener('change', () => {
-    UpdateChart();
-});
-
-
-const toolTipSum = (tooltipItems) => {
-    let sum = 0;    
-    tooltipItems.forEach(function(tooltipItem) {
-        sum += tooltipItem.parsed.y;
-    });
-
-    // round to two decimals
-    sum = Math.round((sum + Number.EPSILON) * 100) / 100
-    return 'Sum: ' + sum;
-};
-
-const toolTipContent = (tooltipItems) => {
-    let label = tooltipItems.dataset.label;
-    let dataOfCategory = tooltipItems.dataset.data[tooltipItems.dataIndex]
-
-    sum = dataOfCategory.sum
-    sum = Math.round((sum + Number.EPSILON) * 100) / 100
-    let details = []
-    details.push(label + ':')
-
-    // sort detail info descending by value
-    dataList = dataOfCategory.values
-    dataList = dataList.sort(function(a, b) {
-        return -(a.value - b.value);
-    });
-    dataList.forEach((entry) => details.push(entry.client + ': ' + entry.value + '\r'))
-
-    return details;
-}
-
-
-_financeDataPool = new FinanceDataPool();
-
-window.electron.onCSVLoaded((data) => {
-    console.log("CSV data received")
-    let csvFile = new CSV_File_DKB(data);
-    _financeDataPool.Add(csvFile);
-    
-    UpdateChart();
-});
-
-
-var _chart
-
-var lastClickTime = performance.now()
-
-const CreateChart = () => {
-    let config = {
-        type: 'bar',
-        options: {
-            maintainAspectRatio: false,
-            responsive: true,
-            interaction: {
-                //intersect: false,
-                mode: 'x'
-            },
-            animation: {
-                duration: 800,
-            },
-            scales: {
-                x: {
-                    stacked: true
-                },
-                y: {
-                    stacked: true
-                }
-            },
-            parsing: {
-                xAxisKey: 'date',
-                yAxisKey: 'sum'
-            },
-            
-            events: ['mousemove', 'mouseout', 'click', 'touchstart', 'touchmove'],
-
-            onClick(event, clickedElements) {
-                chartClickHandler(event, clickedElements)
-            },
-            plugins: {
-                tooltip: {
-                  callbacks: {
-                    afterTitle: toolTipSum
-                    //label: toolTipContent
-                  }
-                },
-                legend: {
-                    onClick: newLegendClickHandler,
-                    labels: {
-                        filter: legendFilter,
-                        sort: legendSorting
-                    }
-                }
-            }
-        }
-    };
-    _chart = new Chart(ctx, config);
-}
-
-const defaultLegendClickHandler = Chart.defaults.plugins.legend.onClick;
-
-const newLegendClickHandler = function (event, legendItem, legend) {
-    let now = performance.now()
-    let doubleClicked = false
-    if (now - lastClickTime < 200)
-        doubleClicked = true
-    lastClickTime = performance.now()
-    
-    const clickedIndex = legendItem.datasetIndex;
-
-    if (!doubleClicked) {
-        defaultLegendClickHandler(event, legendItem, legend);
-    } else {
-        const ci = legend.chart;
-
-        var allOthersAreInvisible = true
-        for (var i=0; i < legend.legendItems.length; i++)
-        {
-            if (i != clickedIndex && ci.isDatasetVisible(i)){
-                allOthersAreInvisible = false
-                break;
-            }
-        }
-
-        for (var i=0; i < legend.legendItems.length; i++){
-            if (allOthersAreInvisible){
-                ci.show(i);
-                legendItem.hidden = false;
-            } else {
-                ci.hide(i);
-                legendItem.hidden = true;
-            }
-        }
-
-        ci.show(clickedIndex);
-        legendItem.hidden = false;
-    }
-}
-
-const legendSorting = function(legendItemA, legendItemB, data){
-    if (legendItemA.text > legendItemB.text)
-        return 1
-    else if (legendItemA.text < legendItemB.text)
-        return -1
-    else
-        return 0;
-}
-
-const legendFilter = function(legendItem, data){
-    var categoryIndex = legendItem.datasetIndex
-    var dataOfCategory = data.datasets[categoryIndex].data
-    return dataOfCategory.length > 0
-}
-
-const chartClickHandler = function(event, clickedElements){
-    if (clickedElements.length === 0) return
-
-    const index = clickedElements[0].index;
-
-    let tableData = []
-    if (_chart.options.interaction.mode == 'x')
-    {
-        let filterDate = _chart.data.datasets[0].data[index].date;
-        if (selectFilter.value == "total")
-            tableData = GetTableDataFromMultipleDatasets(filterDate, onlySums = true)
-        else
-            tableData = GetTableDataFromMultipleDatasets(filterDate, onlySums = false)
-    }
-    else 
-    {
-        const datasetIndex = clickedElements[0].datasetIndex;
-        
-        if (selectFilter.value == "total")
-            tableData = _chart.data.datasets[datasetIndex].data[index]
-        else
-            tableData = _chart.data.datasets[datasetIndex].data[index].values
-    }
-
-
-    UpdateTable(tableData)
-}
-
-const GetTableDataFromMultipleDatasets = (filterDate, onlySums) => 
+const CreateTabs = () => 
 {
-    let tableData = []
-    for(var i=0; i<_chart.data.datasets.length; i++) {
-        let isVisible = _chart.isDatasetVisible(i)
-        if (!isVisible)
-            continue;
+    const modeTabs = document.getElementById('modeTabs');
+    for (const [key, valueSign] of Object.entries(ValueSign))
+    {
+        let btn = document.createElement("button");
+        btn.class = "tabButton";
+        btn.className = "tabButton";
+        btn.id = key
+        btn.innerHTML = key;
+        btn.onclick = () => 
+        { 
+            
+            // Get all elements with class="tabButtons" and remove the class "active"
+            tabButtons = document.getElementsByClassName("tabButton");
+            for (i = 0; i < tabButtons.length; i++) {
+                tabButtons[i].className = tabButtons[i].className.replace(" active", "");
+            }
 
-        let dataset = _chart.data.datasets[i]
-        let categoryData = dataset.data.find((data) => data.date == filterDate);
-        if (categoryData == undefined)
-            continue
-
-        if (onlySums)
-            values = categoryData
-        else
-            values = categoryData.values.filter(x => x != undefined)
-        tableData = tableData.concat(values)
+            // Show the current tab, and add an "active" class to the button that opened the tab
+            document.getElementById(key).style.display = "block";
+            document.getElementById(key).className += " active";
+            _barChart.UpdateChart(_financeDataPool, valueSign);
+        }
+        modeTabs.appendChild(btn);
     }
-    return tableData
 }
 
-const ChangeHoverOption = (option) => {
-    let mode = ""
-    switch(option){
-        case "single":
-            _chart.options.interaction.intersect = true
-            _chart.options.interaction.mode = "nearest"
-            break;
-        case "bar":
-        default:
-            _chart.options.interaction.intersect = true
-            _chart.options.interaction.mode = "x"
-            break;
-    }
-    _chart.update();
-}
 
-const UpdateChart = () => {
-    let categories = _cfgFile.getCategories()
-    let valueSign = ValueSign.NEGATIVE;
-    switch(selectFilter.value){
-        case "expenses" : 
-            valueSign = ValueSign.NEGATIVE;
-            break;
-        case "income" : 
-            valueSign = ValueSign.POSITIVE;
-            break;
-        case "all" : 
-            valueSign = ValueSign.ALL;
-            break;
-        case "total" : 
-            valueSign = ValueSign.TOTAL;
-            break;
-    }
-
-    let ignoredCategories = []
-    if (valueSign == ValueSign.TOTAL)
-        ignoredCategories = ["Hausbau"]
-
-    let categorizedSums = _financeDataPool.getCategorizedGroupedByMonth(categories, valueSign, recreate = true, ignoredCategories = ignoredCategories);
-    let labels = _financeDataPool.getDateLabels()
-
-    _chart.options.scales.x.labels = labels
-    _chart.data.datasets = categorizedSums
-    _chart.update();
-}
-
-var _table
 const CreateTable = () => {
     _table = new Tabulator("#detailsTable", {
         minHeight: 10,
